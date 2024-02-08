f5dd847ec67b5b7ffd1e47d73c45881b8c267d68
MinyeongKim <32475904+MinyeongKim@users.noreply.github.com>
2024-02-08 16:56:02 +0900

    [Bronze II] Title: 문자열 분석, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/10820. 문자열 분석/문자열 분석.py
+++ 백준/Bronze/10820. 문자열 분석/문자열 분석.py
@@ -1,16 +1,16 @@
+import sys
+
 while True:
-    try:
-        str = input()
+    str = sys.stdin.readline().rstrip('\n')
 
-        if not str: break
+    if not str: break
 
-        l, u , d, s =  0, 0, 0, 0
+    l, u , d, s =  0, 0, 0, 0
 
-        for x in str:
-            if x.islower(): l += 1
-            elif x.isupper(): u += 1
-            elif x.isdigit(): d += 1
-            elif x.isspace(): s += 1
+    for x in str:
+        if x.islower(): l += 1
+        elif x.isupper(): u += 1
+        elif x.isdigit(): d += 1
+        elif x.isspace(): s += 1
 
-        print(l, u, d, s)
-    except EOFError: break
+    print(l, u, d, s)
